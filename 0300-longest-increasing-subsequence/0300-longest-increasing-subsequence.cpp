a1712e21c740247b485d01cbfe2b271b4338a2cb
gaurav459-sharma <72649590+gaurav459-sharma@users.noreply.github.com>
2022-10-12 00:56:45 +0530

    Time: 9 ms (95.34%), Space: 10.4 MB (89.25%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
-//     vector<vector<int>>memo;
-//     int helper(vector<int>& nums,int i,int previ){
-        
-//         if(i==nums.size())return 0;
-        
-//         if(nums[i]>tar){
-//             return max(1+helper(nums,i+1,i),helper(nums,i+1,previ));
-//         }
-//         else{
-//             return helper(nums,i+1,tar);
-//         }
-//     }
-    
-//     int lengthOfLIS(vector<int>& nums) {
-//         memo.resize(nums.size(),vector<int>(nums.size()));
-//         return helper(nums,0,-1);
-//     }
-    int lengthOfLIS(vector<int>& nums){
-        vector<int>ans;
-        ans.push_back(nums[0]);
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int>res;
+        res.push_back(nums[0]);
         
         for(int i=1;i<nums.size();i++){
-            if(nums[i]>ans[ans.size()-1]){
-                ans.push_back(nums[i]);
-            }
+            if(nums[i]>res[res.size()-1])res.push_back(nums[i]);
             else{
-                auto lb=lower_bound(ans.begin(),ans.end(),nums[i]);
-                int index=lb-ans.begin();
-                ans[index]=nums[i];
+                auto it=lower_bound(res.begin(),res.end(),nums[i]);
+                int index=it-res.begin();
+                res[index]=nums[i];
             }
         }
-        return ans.size();
+        return res.size();
     }
 };
