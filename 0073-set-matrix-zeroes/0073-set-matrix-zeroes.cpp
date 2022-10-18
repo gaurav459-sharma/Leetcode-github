d042178b6323ec1f1f078c4691525b6ee483e8b9
gaurav459-sharma <72649590+gaurav459-sharma@users.noreply.github.com>
2022-10-18 17:58:12 +0530

    Time: 26 ms (58.33%), Space: 13.2 MB (81.86%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,19 +3,38 @@ public:
     void setZeroes(vector<vector<int>>& mat) {
         int m=mat.size();
         int n=mat[0].size();
-        vector<vector<int>>tmp=mat;
-        for(int i=0;i<m;i++){
+        // vector<vector<int>>tmp=mat;
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(mat[i][j]==0){
+        //             for(int k=0;k<n;k++){
+        //                 tmp[i][k]=0;
+        //             }
+        //             for(int k=0;k<m;k++){
+        //                 tmp[k][j]=0;
+        //             }
+        //         }
+        //     }
+        // }
+        // mat=tmp;
+        vector<pair<int,int>>tmp;
+         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
-                    for(int k=0;k<n;k++){
-                        tmp[i][k]=0;
-                    }
-                    for(int k=0;k<m;k++){
-                        tmp[k][j]=0;
-                    }
+                    tmp.push_back({i,j});
                 }
             }
         }
-        mat=tmp;
+        
+        for(int i=0;i<tmp.size();i++){
+            int r=tmp[i].first;
+            int c=tmp[i].second;
+                   for(int k=0;k<n;k++){
+                        mat[r][k]=0;
+                    }
+                    for(int k=0;k<m;k++){
+                        mat[k][c]=0;
+                    }
+        }
     }
 };
