87840992c60d3cf29f892ff9d65fee46b9bdf74b
gaurav459-sharma <72649590+gaurav459-sharma@users.noreply.github.com>
2022-10-25 12:39:24 +0530

    Added solution - LeetHub

--- Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.cpp
+++ Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.cpp
@@ -16,18 +16,51 @@ class Solution
     {
         // your code here
         // method-1
-        deque<int>dq;
+        // deque<int>dq;
+        // for(int i=0;i<k;i++){
+        //     while(!dq.empty()&&dq.back()<nums[i])dq.pop_back();
+        //     dq.push_back(nums[i]);
+        // }
+        // vector<int>res;
+        // res.push_back(dq.front());
+        // for(int i=k;i<n;i++){
+        //     if(dq.front()==nums[i-k])dq.pop_front();
+        //      while(!dq.empty()&&dq.back()<nums[i])dq.pop_back();
+        //      dq.push_back(nums[i]);
+        //      res.push_back(dq.front());
+        // }
+        // return res;
+        
+        // method-2
+        // priority_queue<pair<int,int>>pq;
+        // for(int i=0;i<k;i++){
+        //     pq.push({nums[i],i});
+        // }
+        // vector<int>res;
+        // res.push_back(pq.top().first);
+       
+        // for(int i=k;i<n;i++){
+        //      pq.push({nums[i],i});
+        //     while(!pq.empty()&&i-pq.top().second>=k)pq.pop();
+           
+        //     res.push_back(pq.top().first);
+        // }
+        // return res;
+        
+        // method-3  by multiset
+        
+        multiset<int>ms;
         for(int i=0;i<k;i++){
-            while(!dq.empty()&&dq.back()<nums[i])dq.pop_back();
-            dq.push_back(nums[i]);
+            ms.insert(nums[i]);
         }
         vector<int>res;
-        res.push_back(dq.front());
+        res.push_back(*ms.rbegin());
+        int si=0;
         for(int i=k;i<n;i++){
-            if(dq.front()==nums[i-k])dq.pop_front();
-             while(!dq.empty()&&dq.back()<nums[i])dq.pop_back();
-             dq.push_back(nums[i]);
-             res.push_back(dq.front());
+           ms.erase(ms.find(nums[si]));
+           si++;
+           ms.insert(nums[i]);
+           res.push_back(*ms.rbegin());
         }
         return res;
        
