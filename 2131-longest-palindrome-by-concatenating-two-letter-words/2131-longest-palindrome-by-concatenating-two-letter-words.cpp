ae598bd93901570077e27fa7a250efb7d7f6d7e1
gaurav459-sharma <72649590+gaurav459-sharma@users.noreply.github.com>
2022-11-03 19:04:49 +0530

    Time: 772 ms (54.61%), Space: 168 MB (33.22%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,44 +1,31 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
-        int len=0;
-        int n=words.size();
-        unordered_map<string,int>mp;
-        for(int i=0;i<n;i++){
-            mp[words[i]]++;
-        }
-        bool take=false;
-        for(int i=0;i<n;i++){
-            string t="";
-            t+=words[i][1];
-            t+=words[i][0];
-           
-            // cout<<mp.size()<<" ";
-            if(t[0]==t[1] && mp.find(t)!=mp.end()){
-                if(mp[t]>=2){
-                    len+=4;
-                    mp[t]-=2;
-                }
-                else if(mp[t]>0&&!take){
-                    len+=2;
-                    // cout<<mp[t];
-                    mp[t]--;
-                    take=true;
-                }
-                
-                if(mp[t]==0){
-                    mp.erase(t);
-                }
-            
+    unordered_map<string, int> m;
+    int unpaired = 0, ans = 0;
+    for (string w: words) {
+        if (w[0] == w[1]) {
+            if (m[w] > 0) {
+                unpaired--;
+                m[w]--;
+                ans += 4;
             }
-            else if(mp.count(words[i])>0 &&mp.count(t)>0){
-                len+=4;
-                mp[words[i]]--;
-                mp[t]--;
-                if(mp[words[i]]==0)mp.erase(words[i]);
-                if(mp[t]==0)mp.erase(t);
+            else {
+                m[w]++;
+                unpaired++;
             }
         }
-        return len;
+        else {
+            string rev = w;
+            reverse(rev.begin(), rev.end());
+            if (m[rev] > 0) {
+                ans += 4;
+                m[rev]--;
+            }
+            else m[w]++;
+        }
     }
+    if (unpaired > 0) ans += 2;
+    return ans;
+}
 };
