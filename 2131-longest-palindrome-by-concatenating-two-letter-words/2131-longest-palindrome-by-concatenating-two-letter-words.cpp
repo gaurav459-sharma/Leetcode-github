6d67d5ab28ee9a4a9e5d78031f425485320a3b91
gaurav459-sharma <72649590+gaurav459-sharma@users.noreply.github.com>
2022-11-03 19:15:54 +0530

    Time: 718 ms (60.94%), Space: 168.3 MB (25.38%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -1,31 +1,73 @@
 class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
-    unordered_map<string, int> m;
-    int unpaired = 0, ans = 0;
-    for (string w: words) {
-        if (w[0] == w[1]) {
-            if (m[w] > 0) {
-                unpaired--;
-                m[w]--;
-                ans += 4;
+        
+        // optimised approach without using map
+        vector<vector<int>>frq(26,vector<int>(26,0));
+        int len=0;
+        int n=words.size();
+        for(auto s:words){
+            int a=s[0]-'a';
+            int b=s[1]-'a';
+            if(frq[b][a]){
+                len+=4;
+                frq[b][a]--;
             }
-            else {
-                m[w]++;
-                unpaired++;
+            else frq[a][b]++;
+        }
+        for(int i=0;i<26;i++){
+            if(frq[i][i]){
+                len+=2;
+                break;
             }
         }
-        else {
-            string rev = w;
-            reverse(rev.begin(), rev.end());
-            if (m[rev] > 0) {
-                ans += 4;
-                m[rev]--;
-            }
-            else m[w]++;
-        }
+        return len;
+        
+        
+        
+        
+        
+        
+        
+        
+//         int len=0;
+//         int n=words.size();
+//         unordered_map<string,int>mp;
+//         for(int i=0;i<n;i++){
+//             mp[words[i]]++;
+//         }
+//         bool take=false;
+//         for(int i=0;i<n;i++){
+//             string t="";
+//             t+=words[i][1];
+//             t+=words[i][0];
+           
+//             // cout<<mp.size()<<" ";
+//             if(t[0]==t[1] && mp.find(t)!=mp.end()){
+//                 if(mp[t]>=2){
+//                     len+=4;
+//                     mp[t]-=2;
+//                 }
+//                 else if(mp[t]>0&&!take){
+//                     len+=2;
+//                     // cout<<mp[t];
+//                     mp[t]--;
+//                     take=true;
+//                 }
+                
+//                 if(mp[t]==0){
+//                     mp.erase(t);
+//                 }
+            
+//             }
+//             else if(mp.count(words[i])>0 &&mp.count(t)>0){
+//                 len+=4;
+//                 mp[words[i]]--;
+//                 mp[t]--;
+//                 if(mp[words[i]]==0)mp.erase(words[i]);
+//                 if(mp[t]==0)mp.erase(t);
+//             }
+//         }
+//         return len;
     }
-    if (unpaired > 0) ans += 2;
-    return ans;
-}
 };
