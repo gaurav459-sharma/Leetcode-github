6b94438de6a5dfc6adb900fefa009589c137215d
gaurav459-sharma <72649590+gaurav459-sharma@users.noreply.github.com>
2022-11-03 15:05:43 +0530

    Time: 1046 ms (25.41%), Space: 174 MB (14.57%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        int len=0;
+        int n=words.size();
+        unordered_map<string,int>mp;
+        for(int i=0;i<n;i++){
+            mp[words[i]]++;
+        }
+        bool take=false;
+        for(int i=0;i<n;i++){
+            string t=words[i];
+            reverse(t.begin(),t.end());
+            // cout<<mp.size()<<" ";
+            if(t[0]==t[1] && mp.size()>0){
+                if(mp[t]>=2){
+                    len+=4;
+                    mp[t]-=2;
+                }
+                else if(mp[t]>0&&!take){
+                    len+=2;
+                    // cout<<mp[t];
+                    mp[t]--;
+                    take=true;
+                }
+                
+                if(mp[t]==0){
+                    mp.erase(t);
+                }
+            
+            }
+            else if(mp.count(words[i])>0 &&mp.count(t)>0){
+                len+=4;
+                mp[words[i]]--;
+                mp[t]--;
+                if(mp[words[i]]==0)mp.erase(words[i]);
+                if(mp[t]==0)mp.erase(t);
+            }
+        }
+        return len;
+    }
+};
